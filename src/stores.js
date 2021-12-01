482f8b06c9db94a39ba66b2b915c40690959dc3d
Thaddeus McCleary <thaddeusmccleary@Thaddeuss-iMac.local>
2021-12-01 08:37:01 -0600

    Scaffold project structure

--- src/stores.js
+++ src/stores.js
@@ -1,10 +1,16 @@
 import { writable } from 'svelte/store';
 
+export const queue = writable([]);
+
+export const state = writable('');
+
+export const colorPreference = writable('');
+
 export const artworkColors = writable({
-	DarkMuted: "#FFFFFF", 
-	DarkVibrant: "#FFFFFF", 
-	LightMuted: "#FFFFFF", 
-	LightVibrant: "#FFFFFF", 
-	Muted: "#FFFFFF", 
+	DarkMuted: "#FFFFFF",
+	DarkVibrant: "#FFFFFF",
+	LightMuted: "#FFFFFF",
+	LightVibrant: "#FFFFFF",
+	Muted: "#FFFFFF",
 	Vibrant: "#FFFFFF"
 });
