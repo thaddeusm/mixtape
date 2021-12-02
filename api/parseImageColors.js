2cada9789eb8737a8bea369a98cd4b3b0647b9ce
Thaddeus McCleary <thaddeusmccleary@Thaddeuss-iMac.local>
2021-12-02 08:12:25 -0600

    Standardize formatting

--- api/parseImageColors.js
+++ api/parseImageColors.js
@@ -1,20 +1,20 @@
-import * as Vibrant from 'node-vibrant'
+import * as Vibrant from 'node-vibrant';
 
 module.exports = async (req, res) => {
-    const photoURL = req.body.photoURL
+    const photoURL = req.body.photoURL;
 
     Vibrant.from(photoURL).getPalette()
         .then((palette) => {
-            let obj = {}
+            let obj = {};
 
             for (var key in palette) {
                 if (palette.hasOwnProperty(key)) {
-                    obj[key] = palette[key].getHex()
+                    obj[key] = palette[key].getHex();
                 }
             }
 
-            return res.status(200).json(obj)
+            return res.status(200).json(obj);
         }).catch((err) => {
-            return res.status(403).json({message: 'error'})
-        })
+            return res.status(403).json({message: 'error'});
+        });
 }
