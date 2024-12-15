461018cbf147706234c3f1aa417fb6df041b9d9f
ELP ITProjects <elpit@georgetown.edu>
2024-12-15 09:21:55 -0600

    Directly replace queue

--- src/components/Playlist.svelte
+++ src/components/Playlist.svelte
@@ -70,33 +70,13 @@
     }
   }
 
-  async function rebuildQueue(songs) {
-    loadingTracks.set(true);
-
-    for (let i=0; i<songs.length; i++) {
-      let obj = {
-        "song": songs[i]
-      };
-
-      if (i == 0) {
-        await music_value.setQueue(obj);
-      } else {
-        await music_value.playLater(obj);
-      }
-    }
-
-    loadingTracks.set(false);
-  }
-
   async function moveUp(index) {
     let newQueue = queue_value;
     let itemToMove = newQueue[index];
     newQueue.splice(index, 1);
     newQueue.splice(index - 1, 0, itemToMove);
 
-    let songIds = newQueue.map(item => item.id);
-
-    await rebuildQueue(songIds);
+    music_value.queue = newQueue;
     queue.set(newQueue);
 
     if (queue_position_value == index) {
@@ -118,9 +98,7 @@
     newQueue.splice(index, 1);
     newQueue.splice(index + 1, 0, itemToMove);
 
-    let songIds = newQueue.map(item => item.id);
-
-    await rebuildQueue(songIds);
+    music_value.queue = newQueue;
     queue.set(newQueue);
 
     if (queue_position_value == index) {
